4e3dea54c4b225df25358c5b6eade29dca82ccd8
PhatHuynh_Flex5 <phathuynh.pth@outlook.com>
2022-05-06 15:29:54 +0200

    Create lucy_ws

---
+++ lucy_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/phathuynh/HBRS_Master/Semester_2/Software_Project/lucy_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/phathuynh/HBRS_Master/Semester_2/Software_Project/lucy_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/phathuynh/HBRS_Master/Semester_2/Software_Project/lucy_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/phathuynh/HBRS_Master/Semester_2/Software_Project/lucy_ws/src:$ROS_PACKAGE_PATH"
---
+++ lucy_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/phathuynh/HBRS_Master/Semester_2/Software_Project/lucy_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
